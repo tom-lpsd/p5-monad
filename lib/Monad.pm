0a1bf3e8dfb2fe1c6bcb0e344e2fa14ba17da566
Tom Tsuruhara <tom.lpsd@gmail.com>
2009-05-14 00:55:55 +0900

    first commit

---
+++ lib/Monad.pm
@@ -0,0 +1,42 @@
+package Monad;
+use Mouse;
+
+=head1 NAME
+
+Monad - Monad with Perl
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+    use Monad;
+
+=cut
+
+=head1 AUTHOR
+
+Tom Tsuruhara, C<< <tom.lpsd at gmail.com> >>
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Monad
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Tom Tsuruhara, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
